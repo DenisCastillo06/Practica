87cbe0d7791a5a568b4d8a256351784945eb6113
DenisCastillo06 <20220365@ricaldone.edu.sv>
2024-11-04 20:27:07 -0600

    navigation actualizado

--- src/navigation/Navigation.js
+++ src/navigation/Navigation.js
@@ -1,21 +1,25 @@
 import {createNativeStackNavigator} from '@react-navigation/native-stack';
 import { NavigationContainer } from '@react-navigation/native';
-
+ 
 import Home from '../screens/Home';
 import Add from '../screens/Add';
-
+import SingUp from '../screens/SignUp';
+import LogIn from '../screens/Login';
+ 
 const Stack = createNativeStackNavigator();
-
+ 
 const Navigation = () => {
     return (
         <NavigationContainer>
-            <Stack.Navigator>
-                <Stack.Screen name="Home" component={Home} options={{title:'Home'}} />
-                <Stack.Screen name="Add" component={Add} 
-                options={{presentation:'modal', title:'Agregar productos'}}/>
+            <Stack.Navigator initialRouteName='Login'>
+                <Stack.Screen name="Home" component={Home} options={{title:'Home', headerShown: false}} />
+                <Stack.Screen name="Add" component={Add}
+                options={{presentation:'modal', title:'Agregar productos', headerShown: false}}/>
+                <Stack.Screen name="Login" component={LogIn} options={{title:'Inicio de sesión', headerShown: false}} />
+                <Stack.Screen name="SignUp" component={SingUp} options={{title:'Registrarse', headerShown: false}} />
             </Stack.Navigator>
         </NavigationContainer>
     );
 }
-
+ 
 export default Navigation;
